3b05ff7a44a69565096ada6b890cf057066ea792
kian maleki <150921250+kianmaleki@users.noreply.github.com>
2025-07-19 13:17:18 +0330

    Update ProfileFacts.tsx

--- src/components/ProfileFacts.tsx
+++ src/components/ProfileFacts.tsx
@@ -4,7 +4,7 @@ export default function ProfileFacts() {
   return (
     <div className="flex flex-wrap justify-center gap-6 sm:gap-12 text-[#A3ABB2] font-bold my-8">
       {[
-        { number: "1", line1: "Years of work", line2: "experience" },
+        { number: "3", line1: "Years of work", line2: "experience" },
         { number: "6+", line1: "Completed", line2: "projects" },
         { number: "6+", line1: "Satisfied", line2: "customers" },
       ].map((fact, index) => (
